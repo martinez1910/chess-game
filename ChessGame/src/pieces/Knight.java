683cbec20ac409b7fb2bb9648861fd061363e416
Martínez <martinez1910@users.noreply.github.com>
2017-11-29 19:07:27 +0100

    Rook and Bishop movements
    Rook and Bishop (both black and white) moves implemented.
    Pawn can now move 2 steps if not moved yet.

--- ChessGame/src/pieces/Knight.java
+++ ChessGame/src/pieces/Knight.java
@@ -11,7 +11,16 @@ public class Knight extends Piece{
 
 	@Override
 	boolean isNewPositionValid(int[] newPosition, Piece[][] piecesBoard) {
-		// TODO Auto-generated method stub
+		int[] position = getPosition();
+		Piece pieceInNewPosition = piecesBoard[newPosition[0]][newPosition[1]];
+		
+		if(pieceInNewPosition != null && pieceInNewPosition.isWhite() == this.isWhite())//Piece of same colour in new position
+			return false;
+		
+		if(Math.abs(position[0] - newPosition[0]) == 2 && Math.abs(position[1] - newPosition[1]) == 1 //Left-Left-Up/Down and Right-Right-Up/Down movements
+				|| Math.abs(position[0] - newPosition[0]) == 1 && Math.abs(position[1] - newPosition[1]) == 2) //Up-Up-Left/Right and Down-Down-Left/Right movements
+			return true;
+		
 		return false;
 	}
 
--- ChessGame/src/pieces/Pawn.java
+++ ChessGame/src/pieces/Pawn.java
@@ -2,7 +2,7 @@ package pieces;
 
 public class Pawn extends Piece{
 
-	
+	private boolean isFirstMove = true;
 
 	public Pawn(boolean isWhite, int[] position) {
 		super(isWhite, position, isWhite ? "\u2659" : "\u265F");
@@ -14,17 +14,33 @@ public class Pawn extends Piece{
 		int[] position = getPosition();
 		Piece pieceInNewPosition = piecesBoard[newPosition[0]][newPosition[1]];
 		if(isWhite()) {
-			if(position[1] == newPosition[1] && position[0] - 1 == newPosition[0] && pieceInNewPosition == null)//forward step if no piece in front
+			if(isFirstMove && position[1] == newPosition[1] && position[0] - 2 == newPosition[0] && pieceInNewPosition == null) {//2 forward steps if no piece in front
+				isFirstMove = false;
 				return true;
-			if((position[1]-1 == newPosition[1] || position[1]+1 == newPosition[1]) && position[0] -1 == newPosition[0] && pieceInNewPosition != null && !pieceInNewPosition.isWhite())//diagonal step if piece there is opposite colour.
+			}
+			if(position[1] == newPosition[1] && position[0] - 1 == newPosition[0] && pieceInNewPosition == null) {//forward step if no piece in front
+				isFirstMove = false;
 				return true;
+			}
+			if((position[1]-1 == newPosition[1] || position[1]+1 == newPosition[1]) && position[0] -1 == newPosition[0] && pieceInNewPosition != null && !pieceInNewPosition.isWhite()) {//diagonal step if piece there is opposite colour.
+				isFirstMove = false;
+				return true;
+			}
 		}
 		
 		if(!isWhite()) {
-			if(position[1] == newPosition[1] && position[0] + 1 == newPosition[0] && pieceInNewPosition == null)//forward step if no piece in front
+			if(isFirstMove && position[1] == newPosition[1] && position[0] + 2 == newPosition[0] && pieceInNewPosition == null) {//2 forward steps if no piece in front
+				isFirstMove = false;
 				return true;
-			if((position[1] - 1 == newPosition[1] || position[1] + 1 == newPosition[1]) && position[0] + 1 == newPosition[0] && pieceInNewPosition != null && pieceInNewPosition.isWhite())//diagonal step if piece there is opposite colour.
+			}
+			if(position[1] == newPosition[1] && position[0] + 1 == newPosition[0] && pieceInNewPosition == null) {//forward step if no piece in front
+				isFirstMove = false;
 				return true;
+			}
+			if((position[1] - 1 == newPosition[1] || position[1] + 1 == newPosition[1]) && position[0] + 1 == newPosition[0] && pieceInNewPosition != null && pieceInNewPosition.isWhite()) {//diagonal step if piece there is opposite colour.
+				isFirstMove = false;
+				return true;
+			}
 		}
 		
 		return false;
